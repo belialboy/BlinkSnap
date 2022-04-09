2b33eeda83fc9dcbcb07e9b8ad7d7ec94cc8b0a0
EC2 Default User <ec2-user@ip-172-31-16-156.eu-west-1.compute.internal>
2022-04-09 17:07:23 +0000

    reducing writes to disk and making gif size even smaller.

--- src/movie/lambda_function.py
+++ src/movie/lambda_function.py
@@ -6,6 +6,7 @@ import datetime
 from PIL import Image
 from PIL import ImageDraw
 from twython import Twython
+import io
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,16 +17,15 @@ def lambda_handler(event, context):
     
     my_date = datetime.date.today()
     year, week_num, day_of_week = my_date.isocalendar()
-    print(os.system("ls /opt/ > /tmp/ls.txt"))
-    file = open("/tmp/ls.txt", 'rb')
-    print(file.read())
+
     s3 = boto3.resource('s3')
     bucket_name = os.environ['outputBucket']
+    GIF_SIZE = (426,240)
 
     my_bucket = s3.Bucket(bucket_name)
     
-    tmp = "/tmp/{index}.tmp"
-    out = "/tmp/{year}-{week}-output.mp4"
+    tmp = "/tmp/giffy.jpg"
+    out = "/tmp/{year}-{week}-output.gif"
     images = []
     
     for my_bucket_object in my_bucket.objects.all():
@@ -34,18 +34,20 @@ def lambda_handler(event, context):
     
             if (file_year == year and file_week_num == week_num):
                 logger.info("Reading {} ({})".format(my_bucket_object.key,len(images)+1))
-                my_bucket.download_file(my_bucket_object.key, tmp)
-                logger.info("Creating Image object from downloaded file")
-                raw_image = Image.open(tmp)
+                
+                file_stream = io.BytesIO()
+                object = my_bucket.Object(my_bucket_object.key)
+                object.download_fileobj(file_stream)
+                raw_image = Image.open(file_stream)
+                
                 logger.info("Resizing file to more managable dimensions")
-                resized = raw_image.resize((426,240),reducing_gap=2.0)
+                resized = raw_image.resize(GIF_SIZE,reducing_gap=2.0)
                 ImageDraw.Draw(resized).text((0, 0), my_bucket_object.last_modified.strftime("%A"), (0, 0, 0))
                 images.append(resized)
                 
     logger.info("Got all the images I need for {} week {}".format(year,week_num))
     out_file = out.format(year=year,week=week_num)
-    #write_out(images,out_file,my_bucket)
-    os.system("/opt/bin/ffmpeg -framerate 2 -pattern_type glob -i '{infiles}' \ -c:v libx264 -pix_fmt yuv420p {outfile}".format(infiles=tmp.format(index="*"),outfile=out_file))
+    write_out(images,out_file,my_bucket)
     
     logger.info("Creating a twitter client")
     twitter = Twython(os.environ["twitterConsumerKey"], 
